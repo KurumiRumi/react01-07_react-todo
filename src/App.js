27aa3f9d4864e922f7d1dfaf8f6820eba9e41c5c
KurumiRumi <40328870+KurumiRumi@users.noreply.github.com>
2022-07-22 16:20:55 +0900

    52. CSSでスタイリング

--- src/App.js
+++ src/App.js
@@ -4,30 +4,30 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+      <div className="input-area">
+        <input id="add-text" placeholder="TODOを入力" />
+        <button id="add-button">追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
-        <ul>
-          <li>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul id="incomplete-list">
+          <li className="list-row">
             <div>TODOです。</div>
             <button>完了</button>
             <button>削除</button>
           </li>
-          <li>
+          <li className="list-row">
             <div>TODOです。</div>
             <button>完了</button>
             <button>削除</button>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了したTODO</p>
-        <ul>
-          <li>
-            <div>TODOでした。</div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul id="complete-list">
+          <li className="list-row">
+            <div>TODOでした</div>
             <button>戻す</button>
           </li>
         </ul>
